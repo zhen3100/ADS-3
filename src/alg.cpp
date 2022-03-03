fbff4ce8eb79b78af22c31b2eed042efaf34dad4
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-03-03 22:39:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int k = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            k++;
+        }
+    }
+    if (k > 0) {
+        return k;
+    }
+    else {
+        return 0;
+    }
 }
