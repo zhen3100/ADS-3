73ef290af848060cf5d83ad57ce878e4bef2e1fe
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-03-10 16:09:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,26 @@
 // Copyright 2021 EV.VOLKOV
 
 int cbinsearch(int *arr, int size, int value) {
-    int k = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
-            k++;
+    int i = 0, j = size - 1;
+    int count = 0;
+    while (i < j) {
+        int mid = i + (j - i) / 2;
+        if (arr[mid] == value) { 
+            int m = mid;
+            while (arr[m] == value) {
+                ++count;
+                ++m;
+            }
+            while (arr[mid - 1] == value) {
+                ++count;
+                --mid;
+            }
+            return count;
+        } else if (arr[mid] > value) {
+            j = mid;
+        } else {
+            i = mid + 1;
         }
     }
-    if (k > 0) {
-        return k;
-    } else { return 0; }
+    return 0;
 }
